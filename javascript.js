15ebec3a3d09441367b6648a3b4de7bf09751d18
J <jpham26@gmail.com>
2023-10-16 16:37:42 -0700

    Refactor gameBoardModule and gameFlowModule Add winner announcement

--- javascript.js
+++ javascript.js
@@ -2,35 +2,95 @@
 
 // ======== PLAYERS LOGIC (FACTORY FUNCTION) ========== 
 
+
 // Create a factory function for Players
 function Player(name, marker) {
   return { name, marker };
 }
 
-// Create players, will have to get names from form eventually
-const playerOne = Player('Stacey', 'X');
-const playerTwo = Player('Jack', 'O');
+
+function createPlayers(playerOneName, playerTwoName) {
+  // Get names from the form and create players
+  const playerOne = Player(playerOneName, 'X');
+  const playerTwo = Player(playerTwoName, 'O');
+
+  return { playerOne, playerTwo };
+}
+
+
+const formElement = document.querySelector('#player-name-input');
+
+formElement.addEventListener('submit', function (event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get values from the form fields
+  const playerOneName = document.querySelector('#player-one-name').value;
+  const playerTwoName = document.querySelector('#player-two-name').value;
+
+  // Create players 
+  const players = createPlayers(playerOneName, playerTwoName);
+
+  // Start the game
+  gameFlowModule.startGame(players);  
+})
 
 
 // ======== GAMEBOARD LOGIC (MODULE PATTERN) ========== 
 
-// Create a module pattern for gameBoard
-let gameBoard = {
-  1: " ", 2: " ", 3: " ",
-  4: " ", 5: " ", 6: " ",
-  7: " ", 8: " ", 9: " "
-};
+const gameBoardModule = (function() {
+  let board = {
+    1: " ", 2: " ", 3: " ",
+    4: " ", 5: " ", 6: " ",
+    7: " ", 8: " ", 9: " "
+  };
 
-// Function to display the board
+  function getBoard() {
+    return board;
+  }
+
+  function setCell(cellId, marker) {
+    if(board[cellId] === " ") {
+      board[cellId] = marker;
+    }
+  }
+
+  function resetBoard () {
+    board = {
+      1: " ", 2: " ", 3: " ",
+      4: " ", 5: " ", 6: " ",
+      7: " ", 8: " ", 9: " "
+    };
+  }
+
+  return {
+    getBoard,
+    setCell,
+    resetBoard
+  };
+})();
+
+// Initialize the board
+gameBoardModule.resetBoard();
+
+// Function to display the board on the UI
 function displayBoard() {
-  // Get a reference to the game board container element
-  const containerElement = document.querySelector('.game-board');
+  const board = gameBoardModule.getBoard();
+
+  // Get a reference of the container that holds the game board
+  const playGameContainer = document.querySelector('.play-game-container');
+
+  // Get a reference to the game board element
+  const gameBoardElement = document.querySelector('.game-board');
+  
+  // Show the game board UI container
+  playGameContainer.style.display = 'flex';
 
   // Clear the board
-  containerElement.innerHTML = '';
+  gameBoardElement.innerHTML = '';
 
   //Loop through the board object and update the cells
-  Object.keys(gameBoard).forEach(cell => {
+  Object.keys(board).forEach(cell => {
     const newCell = document.createElement('div');
 
     // Set the class attribute
@@ -40,126 +100,126 @@ function displayBoard() {
     newCell.dataset.cellId = cell;
 
     //HTML structure of the new cells
-    newCell.innerHTML = `<button class="marker-button">${gameBoard[cell]}</button>`;
+    newCell.innerHTML = `<button class="marker-button">${board[cell]}</button>`;
 
     // Add the new cell
-    containerElement.appendChild(newCell);
+    gameBoardElement.appendChild(newCell);
   })
 }
 
 // Attach the event listeners for the cells when the page loads
 window.addEventListener('load', attachCellButtonListeners);
 
-// Function to attach event listener for the gameBoard
-function attachCellButtonListeners() {
-  const containerBoard = document.querySelector('.game-board');
-  containerBoard.addEventListener('click', function (event) {
-    if (event.target.classList.contains('marker-button')) {
+// Get the reference for the dialog modal
+const dialog = document.querySelector("dialog");
 
-      // Get the cell div from the button's parent 
+function attachCellButtonListeners() {
+  const gameBoardContainer = document.querySelector('.game-board');
+  gameBoardContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('marker-button')) {
+      
+      // Get the cell div from the parent element
       const cellDiv = event.target.parentElement;
 
       // Get the cell ID from the data-cell-id attribute of the cell div
-      const cellId = cellDiv.dataset.cellId;
+      const cellId = parseInt(cellDiv.dataset.cellId);
 
-      // Update the button's innerHTML
-      event.target.innerHTML = addMarkerToBoard(parseInt(cellId));
+      const currentPlayer = gameFlowModule.getCurrentPlayer();
+
+      gameBoardModule.setCell(cellId, currentPlayer.marker);
+
+      // Update the innerHTML with current player's marker
+      event.target.innerHTML = gameFlowModule.addMarkerToBoard(cellId);
+
+      gameFlowModule.togglePlayer();
 
       displayBoard();
-
-      const winner = checkForWinner();
+      
+      const winnerElement = document.querySelector('.winner-announcement');
+      const winner = gameFlowModule.checkForWinner();
       if (winner) {
-        console.log("We have a winner!");
-      }
-      // Need to endgame if winner or tie
-    }
+        winnerElement.innerHTML = `${winner}`;
+        dialog.showModal();
+      }    
+    }  
   });
 }
-
-// Initiate the game for testing
-displayBoard();
-
-// Restart game
-const restartButtons = document.querySelectorAll('.restart-button');
-restartButtons.forEach((button) => {
-  button.addEventListener('click', function (event) {
-    restartGame();
-  });
-});
-
-function restartGame() {
-  gameBoard = {
-    1: " ", 2: " ", 3: " ",
-    4: " ", 5: " ", 6: " ",
-    7: " ", 8: " ", 9: " "
-  };
-  displayBoard();
-}
-
-function startGame() {
-  displayBoard();
-}
-
-function endGame() {
-  //Stuff goes here
-}
-
 
 
 // ======== GAMEFLOW LOGIC (MODULE PATTERN) ========== 
 
-// Initialize the current player
-let currentPlayer = playerOne;
+const gameFlowModule = (function() {
+  let playerOne;
+  let playerTwo;
+  let currentPlayer;
 
-function addMarkerToBoard(cellId) {
-
-  if (gameBoard[cellId] === " ") {
-
-    // Add the marker to the board
-    gameBoard[cellId] = currentPlayer.marker;
-  } else {
-    return;
+  function startGame(players) {
+    playerOne = players.playerOne;
+    playerTwo = players.playerTwo;
+    currentPlayer = playerOne;
+    displayBoard();
   }
 
-  // Toggle current player
-  currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
-
-  return gameBoard[cellId];
-}
-
-// Create a module for gameFlow
-
-
-// Create function that checks when the game is over, 3 in a row + tie
-function checkCells(a, b, c, marker) {
-  return gameBoard[a] === marker && gameBoard[b] === marker && gameBoard[c] === marker;
-}
-
-function checkForWinner() {
-  if (
-    checkCells(1, 2, 3, playerOne.marker) ||
-    checkCells(4, 5, 6, playerOne.marker) ||
-    checkCells(7, 8, 9, playerOne.marker) ||
-    checkCells(1, 5, 9, playerOne.marker) ||
-    checkCells(3, 5, 7, playerOne.marker)
-  ) {
-    console.log(`${playerOne.name} wins!`);
-    return `${playerOne.name} wins!`;
-
-  } else if (
-    checkCells(1, 2, 3, playerTwo.marker) ||
-    checkCells(4, 5, 6, playerTwo.marker) ||
-    checkCells(7, 8, 9, playerTwo.marker) ||
-    checkCells(1, 5, 9, playerTwo.marker) ||
-    checkCells(3, 5, 7, playerTwo.marker)
-  ) {
-    console.log(`${playerTwo.name} wins!`);
-    return `${playerTwo.name} wins!`;
+  function restartGame() {
+    gameBoardModule.resetBoard();
+    displayBoard();
   }
 
-  // Check for a tie
-  if (Object.values(gameBoard).every(cell => cell !== " ")) {
-    console.log("It's a tie!");
-    return "It's a tie!";
+  // Export togglePlayer
+  function togglePlayer() {
+    currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
   }
-}
+
+  // Export getCurrentPlayer for use in main code
+  function getCurrentPlayer() {
+    return currentPlayer;
+  }
+
+  function addMarkerToBoard(cellId) {
+    gameBoardModule.setCell(cellId, currentPlayer.marker);
+    return currentPlayer.marker;
+  }
+
+  function checkCells(a, b, c, marker) {
+    const board = gameBoardModule.getBoard();
+    return board[a] === marker && board[b] === marker && board[c] === marker;
+  }
+
+  function checkForWinner() {
+    if (
+      checkCells(1, 2, 3, playerOne.marker) ||
+      checkCells(4, 5, 6, playerOne.marker) ||
+      checkCells(7, 8, 9, playerOne.marker) ||
+      checkCells(1, 5, 9, playerOne.marker) ||
+      checkCells(3, 5, 7, playerOne.marker)
+    ) {
+      return `${playerOne.name} wins!`;
+  
+    } else if (
+      checkCells(1, 2, 3, playerTwo.marker) ||
+      checkCells(4, 5, 6, playerTwo.marker) ||
+      checkCells(7, 8, 9, playerTwo.marker) ||
+      checkCells(1, 5, 9, playerTwo.marker) ||
+      checkCells(3, 5, 7, playerTwo.marker)
+    ) {
+      return `${playerTwo.name} wins!`;
+    }
+
+    // Check for a tie
+    const board = gameBoardModule.getBoard();
+    if (Object.values(board).every(cell => cell !== " ")) {
+      return "It's a draw.";
+    }
+  }
+
+  return {
+    startGame,
+    restartGame,
+    getCurrentPlayer,
+    togglePlayer,
+    addMarkerToBoard,
+    checkForWinner,
+  };
+
+})();
+
